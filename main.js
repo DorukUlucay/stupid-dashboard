3af1fa04d00a06c5e2b80f1271e641d0496f1c1d
Doruk Uluçay <doruk.ulucay@ingamegroup.com>
2019-12-04 19:37:33 +0300

    add clock

--- main.js
+++ main.js
@@ -8,15 +8,37 @@ var defaults = {
 }
 
 $(function () {
+    update();
+
     $(".draggable").draggable({
         start: function () {
-            
+
         },
         drag: function () {
-            
+
         },
         stop: function () {
 
         }
     });
 })
+
+
+function update() {
+    setTimeout(() => {
+
+        $("#systemdate").text(getsystemdate());
+        $("#utcdate").text(getutcdate());
+        update();
+    }, 1000);
+}
+
+function getsystemdate() {
+    return new Date();
+}
+
+function getutcdate() {
+    return new Date().toUTCString();
+}
+
+
